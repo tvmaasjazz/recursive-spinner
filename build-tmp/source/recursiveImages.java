59fe276be9e75e6aade3e603c3d694f0fb87f00b
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-11-27 12:25:26 -0500

    Change displaySpinner to createSpinner

--- build-tmp/source/recursiveImages.java
+++ build-tmp/source/recursiveImages.java
@@ -25,18 +25,21 @@ public void setup() {
 public void draw() {
   background(255);
 
-  displaySpinner(width/2, height/2, 600, 0.00005f);
+  // generate an array of all of them
+  createSpinner(width/2, height/2, 600, 0.00005f);
+
+  // loop through and display them
 }
 
-public void displaySpinner(float x, float y, float radius, float spinRate) {
+public void createSpinner(float x, float y, float radius, float spinRate) {
   Spinner s = new Spinner(x, y, radius, 1, 10, spinRate);
   s.display();
 
   if (radius > 200) {
-    displaySpinner(x + radius / 2, y, radius / 2, spinRate * 3);
-    displaySpinner(x, y + radius / 2, radius / 2, spinRate * 3);
-    displaySpinner(x - radius / 2, y, radius / 2, spinRate * 3);
-    displaySpinner(x, y - radius / 2, radius / 2, spinRate * 3);
+    createSpinner(x + radius / 2, y, radius / 2, spinRate * 3);
+    createSpinner(x, y + radius / 2, radius / 2, spinRate * 3);
+    createSpinner(x - radius / 2, y, radius / 2, spinRate * 3);
+    createSpinner(x, y - radius / 2, radius / 2, spinRate * 3);
   }
 }
 class Spinner {
@@ -56,7 +59,7 @@ class Spinner {
     float tempSpinRate = spinRate;
     float tempRadius = radius;
 
-    while (tempRadius > 10) {
+    while (tempRadius > 10  ) {
       float spinPosition = millis() * tempSpinRate;
 
       for (int i = 0; i < sections; i++) {
--- recursiveImages.pde
+++ recursiveImages.pde
@@ -9,17 +9,20 @@ void setup() {
 void draw() {
   background(255);
 
-  displaySpinner(width/2, height/2, 600, 0.00005);
+  // generate an array of all of them
+  createSpinner(width/2, height/2, 600, 0.00005);
+
+  // loop through and display them
 }
 
-void displaySpinner(float x, float y, float radius, float spinRate) {
+void createSpinner(float x, float y, float radius, float spinRate) {
   Spinner s = new Spinner(x, y, radius, 1, 10, spinRate);
   s.display();
 
   if (radius > 200) {
-    displaySpinner(x + radius / 2, y, radius / 2, spinRate * 3);
-    displaySpinner(x, y + radius / 2, radius / 2, spinRate * 3);
-    displaySpinner(x - radius / 2, y, radius / 2, spinRate * 3);
-    displaySpinner(x, y - radius / 2, radius / 2, spinRate * 3);
+    createSpinner(x + radius / 2, y, radius / 2, spinRate * 3);
+    createSpinner(x, y + radius / 2, radius / 2, spinRate * 3);
+    createSpinner(x - radius / 2, y, radius / 2, spinRate * 3);
+    createSpinner(x, y - radius / 2, radius / 2, spinRate * 3);
   }
 }
